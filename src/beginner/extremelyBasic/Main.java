447aa39bdffbf0a2015c236fbaa9dde13df0961c
pedro <pedroflopes1710@gmail.com>
2024-10-15 09:30:36 -0300

    1001 - Extremely Basic

---
+++ src/beginner/extremelyBasic/Main.java
@@ -0,0 +1,20 @@
+package beginner.extremelyBasic;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
+        sc.close();
+    }
+}
