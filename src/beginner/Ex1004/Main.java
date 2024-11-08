426385a45b1dd366af881c7474cb57df43e7fda3
pedro <pedroflopes1710@gmail.com>
2024-11-08 15:57:49 -0300

    1004 - Simple Product

---
+++ src/beginner/Ex1004/Main.java
@@ -0,0 +1,21 @@
+package beginner.Ex1004;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int prod = a * b;
+
+        System.out.println("PROD = " + prod);
+
+        sc.close(); 
+    }
+ 
+}
