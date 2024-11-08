7e87ea642da33035e827e88123be34e6f2da695f
pedro <pedroflopes1710@gmail.com>
2024-11-08 15:29:05 -0300

    1000 - Hello World!

---
+++ src/beginner/Ex1000/Main.java
@@ -0,0 +1,10 @@
+package beginner.Ex1000;
+
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+           System.out.println("Hello World!"); 
+    }
+}
