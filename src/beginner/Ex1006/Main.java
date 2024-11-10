9d03817d0d3b80c73ca37132442b62612e3c64bf
pedro <pedroflopes1710@gmail.com>
2024-11-09 23:12:40 -0300

    1006 - Average 2

---
+++ src/beginner/Ex1006/Main.java
@@ -0,0 +1,22 @@
+package beginner.Ex1006;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double C = scanner.nextDouble();
+
+        double media = (A * 2 + B * 3 + C * 5) / 10;
+
+        System.out.printf("MEDIA = %.1f%n", media);
+
+        scanner.close();
+    }
+}
+
